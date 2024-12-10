cbde6b238fced3eb0c813ccb0e16e67f86c0d0e5
wispl <wispl.8qbkk@slmail.me>
2024-12-10 03:21:50 -0500

    cleanup: rename home-manager to home

--- flake.nix
+++ flake.nix
@@ -31,7 +31,7 @@
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.users.wisp = import ./home-manager/home.nix;
+            home-manager.users.wisp = import ./home/home.nix;
           }
         ];
       };
