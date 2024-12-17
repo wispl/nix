16f549ca6e24bc59a17cfadbc3f4df9294bc9d66
wispl <wispl.8qbkk@slmail.me>
2024-12-17 14:58:50 -0500

    nvim: add grug-far

--- nvim/lua/plugins/edit.lua
+++ nvim/lua/plugins/edit.lua
@@ -90,5 +90,24 @@ return {
 				end
 			},
 		}
+	},
+	-- search and replace
+	{
+		"MagicDuck/grug-far.nvim",
+		cmd = "GrugFar",
+		keys = {
+			{
+				"<leader>s",
+				function()
+					require("grug-far").open({ prefills = {
+						search = vim.fn.expand("<cword>"),
+						paths = vim.fn.expand("%")
+					} })
+				end,
+				mode = { "n", "v" },
+				desc = "Search and Replace",
+			},
+		},
+		opts = {},
 	}
 }
