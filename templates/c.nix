3c2d34f08bf93b87ee84936032b6073f3f470b43
wispl <wispl.8qbkk@slmail.me>
2024-12-17 14:58:00 -0500

    nix: initial templates
    Should maybe use nix flake init instead?

---
+++ templates/c.nix
@@ -0,0 +1,28 @@
+{
+  description = "Nix flake template for C";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    supportedSystems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+    forEachSupportedSystem = f:
+      nixpkgs.lib.genAttrs supportedSystems (system:
+        f {
+          pkgs = import nixpkgs {inherit system;};
+        });
+  in {
+    devShells = forEachSupportedSystem ({pkgs}: {
+      default = pkgs.mkShell {
+        packages = with pkgs; [
+          # add packages
+        ];
+        env = {
+          # change env
+        };
+      };
+    });
+  };
+}
