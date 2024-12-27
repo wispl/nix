00857ed19704890f34e6dba66c806799267177fc
wispl <wispl.8qbkk@slmail.me>
2024-12-27 01:47:40 -0500

    nvim: add auto root

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -78,3 +78,27 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.opt_local.spell = true
   end,
 })
+
+-- Good enough
+local root_names = { ".git", "Makefile" }
+local root_cache = {}
+
+vim.api.nvim_create_autocmd("BufEnter", {
+	group = vim.api.nvim_create_augroup("auto_root", {}),
+	callback = function()
+		local path = vim.api.nvim_buf_get_name(0)
+		if path == "" then return end
+		path = vim.fs.dirname(path)
+
+		local root = root_cache[path]
+		if root == nil then
+			local root_file = vim.fs.find(root_names, { path = path, upward = true })[1]
+			if root_file == nil then return end
+			root = vim.fs.dirname(root_file)
+			root_cache[path] = root
+		end
+
+		vim.notify("Auto cding to " .. root)
+		vim.cmd.tchdir(root)
+	end
+})
