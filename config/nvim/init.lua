44487f0ac0856ec22cbd7a8ad3d9fff5020f63bc
wispl <wispl.8qbkk@slmail.me>
2025-12-28 19:33:34 -0500

    nvim: adapt to treesitter main branch

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -16,50 +16,52 @@ vim.opt.clipboard = ""
 -- bootstrap lazy
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable",
-		lazypath,
-	})
+    vim.fn.system({
+	"git",
+	"clone",
+	"--filter=blob:none",
+	"https://github.com/folke/lazy.nvim.git",
+	"--branch=stable",
+	lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- install plugins
 require("lazy").setup("plugins", {
-	performance = {
-		cache = { enabled = true },
-		rtp = {
-			disabled_plugins = {
-				"gzip",
-				"tarPlugin",
-				"tohtml",
-				"tutor",
-				"rplugin",
-				"zipPlugin",
-				"matchit",
-				"netrwPlugin"
-			},
-		}
-	},
+    -- nixos, need better solution eventually
+    lockfile = os.getenv("FLAKE") .. "/lazy-lock.json",
+    performance = {
+	cache = { enabled = true },
+	rtp = {
+	    disabled_plugins = {
+		"gzip",
+		"tarPlugin",
+		"tohtml",
+		"tutor",
+		"rplugin",
+		"zipPlugin",
+		"matchit",
+		"netrwPlugin"
+	    },
+	}
+    },
 })
 
 --- source rest of configs
 vim.api.nvim_create_autocmd("User", {
-	group = vim.api.nvim_create_augroup("SourceConfig", { clear = true }),
-	pattern = "VeryLazy",
-	callback = function()
-		config("keymaps.lua")
-		config("autocmds.lua")
+    group = vim.api.nvim_create_augroup("SourceConfig", { clear = true }),
+    pattern = "VeryLazy",
+    callback = function()
+	config("keymaps.lua")
+	config("autocmds.lua")
 
-		vim.o.shada = shada
-		pcall(vim.cmd.rshada, { bang = true })
+	vim.o.shada = shada
+	pcall(vim.cmd.rshada, { bang = true })
 
-		vim.opt.clipboard = clipboard
-		-- TODO: Not too sure about this, going to try it out a little
-		vim.cmd.packadd("matchit")
-	end
+	vim.opt.clipboard = clipboard
+	-- TODO: Not too sure about this, going to try it out a little
+	vim.cmd.packadd("matchit")
+    end
 })
 vim.cmd.colorscheme("kanagawa")
--- config/nvim/lua/plugins/treesitter.lua
+++ config/nvim/lua/plugins/treesitter.lua
@@ -1,46 +1,37 @@
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		branch = "main",
-		lazy = false,
-		build = ":TSUpdate",
-		config = function()
-			local ts = require("nvim-treesitter")
-			ts.install({
-				"bash",
-				"c",
-				"cmake",
-				"cpp",
-				"css",
-				"glsl",
-				"html",
-				"javascript",
-				"json",
-				"jsonc",
-				"lua",
-				"markdown",
-				"markdown_inline",
-				"nix",
-				"python",
-				"regex",
-				"rust",
-				"vim",
-				"yuck",
-				"zig"
-			})
+    {
+	"nvim-treesitter/nvim-treesitter",
+	branch = "main",
+	lazy = false,
+	build = ":TSUpdate",
+	config = function()
+	    local ts = require("nvim-treesitter")
+	    vim.api.nvim_create_autocmd("User", {
+		group = vim.api.nvim_create_augroup("install_treesitter", { clear = true} ),
+		pattern = "LazySync",
+		once = true,
+		callback = function()
+		    ts.install({
+			"bash", "c", "cmake", "cpp", "css", "glsl",
+			"html", "javascript", "json", "jsonc", "lua", "markdown",
+			"markdown_inline", "nix", "python", "regex", "rust", "vim",
+			"yuck", "zig"
+		    })
 		end
+	    })
+	end
+    },
+    -- latex
+    {
+	"lervag/vimtex",
+	lazy = false,
+	version = "v2.16",
+	keys = {
+	    { "<leader>lf", '<cmd>silent execute "!inkfig " . b:vimtex.root . "/figures/<cword>.svg"<cr>',  silent = true },
 	},
-	-- latex
-	{
-		"lervag/vimtex",
-		lazy = false,
-		version = "v2.16",
-		keys = {
-			{ "<leader>lf", '<cmd>silent execute "!inkfig " . b:vimtex.root . "/figures/<cword>.svg"<cr>',  silent = true },
-		},
-		config = function()
-			vim.g.vimtex_view_method = "sioyek"
-			vim.g.vimtex_format_enabled = 1
-		end,
-	}
+	config = function()
+	    vim.g.vimtex_view_method = "sioyek"
+	    vim.g.vimtex_format_enabled = 1
+	end,
+    }
 }
