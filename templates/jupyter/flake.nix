48940ad7c30e721c9b844a74f8386c74a61d6643
wispl <wispl.8qbkk@slmail.me>
2026-01-08 01:18:10 -0500

    nix: new template

---
+++ templates/jupyter/flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "Nix flake template for Python";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.systems.url = "github:nix-systems/default";
+  inputs.flake-utils = {
+    url = "github:numtide/flake-utils";
+    inputs.systems.follows = "systems";
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+        # change python version here
+        python = pkgs.python313;
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            (python.withPackages (python-pkgs: [
+              # python packages go here
+              ipykernel
+              jupytext
+            ]))
+          ];
+          # maybe?
+          # shellHook = ''
+          # 	export PIP_PREFIX=$(pwd)/_build/pip_packages #Dir where built packages are stored
+          # 	export PYTHONPATH="$PIP_PREFIX/${pkgs.python3.sitePackages}:$PYTHONPATH"
+          # 	export PATH="$PIP_PREFIX/bin:$PATH"
+          # 	unset SOURCE_DATE_EPOCH
+          # '';
+        };
+      }
+    );
+}
