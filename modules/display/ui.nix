008ecaa750ed6739d2f6f32ebb99957c0572eb0f
wispl <wispl.8qbkk@slmail.me>
2026-06-21 15:33:04 -0400

    nix: remove waybar
    I have not used or tested this in a looooong time, it might be broken

--- modules/display/ui.nix
+++ modules/display/ui.nix
@@ -9,7 +9,6 @@
 in {
   options.modules.desktop.ui = {
     fuzzel.enable = mkEnableOption "fuzzel";
-    waybar.enable = mkEnableOption "waybar";
     eww.enable = mkEnableOption "eww";
     quickshell.enable = mkEnableOption "quickshell";
     mako.enable = mkEnableOption "mako";
@@ -48,40 +47,6 @@ in {
             border = "${config.colors.bgL}ff";
           };
         };
-      };
-    })
-
-    # only bar
-    (mkIf cfg.waybar.enable {
-      home.packages = [pkgs.waybar];
-      home.xdg.config.files = {
-        "waybar/config.jsonc".source = lib.importJSON ../../config/waybar/pills/config.jsonc;
-        "waybar/style.css".text = ''
-          @define-color fg #${config.colors.fg};
-
-          @define-color bg0 #${config.colors.bgDDD};
-          @define-color bg1 #${config.colors.bgDD};
-          @define-color bg2 #${config.colors.bgD};
-          @define-color bg  #${config.colors.bg};
-          @define-color bg4 #${config.colors.bgL};
-          @define-color bg5 #${config.colors.bgLL};
-          @define-color bg6 #${config.colors.bgLLL};
-
-          @define-color red     #${config.colors.red};
-          @define-color green   #${config.colors.green};
-          @define-color yellow  #${config.colors.yellow};
-          @define-color blue    #${config.colors.blue};
-          @define-color magenta #${config.colors.magenta};
-          @define-color cyan    #${config.colors.cyan};
-
-          @define-color bright-red     #${config.colors.brightRed};
-          @define-color bright-green   #${config.colors.brightGreen};
-          @define-color bright-yellow  #${config.colors.brightYellow};
-          @define-color bright-blue    #${config.colors.brightBlue};
-          @define-color bright-magenta #${config.colors.brightMagenta};
-          @define-color bright-cyan    #${config.colors.brightCyan};
-                 ${builtins.readFile ../../config/waybar/pills/style.css}
-        '';
       };
     })
 
